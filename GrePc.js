f8fcd2d2b4afca9984d0af219bd6eaa7bc30dab8
GrePC <64420953+GrePC@users.noreply.github.com>
2020-10-30 01:07:06 -0500

    added dropdown option links, temoplates, and routed pages.

--- GrePc.js
+++ GrePc.js
@@ -1,6 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector(".burger-btn").addEventListener("click", function() {
-        document.querySelectorAll('*[class^="burger"').forEach( item=> item.classList.toggle("show")
-        )
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".menu-btn").addEventListener("click", function () {
+        document.querySelectorAll('*[class^="menu"]').forEach(item => item.classList.toggle("show"))
     })
-}) 
+})
+document.addEventListener("DOMContentLoaded", function () {
+    const selected = document.querySelector(".selected");
+    const optionsContainer = document.querySelector(".options-container");
+
+    const optionsList = document.querySelectorAll(".option");
+
+    selected.addEventListener("click", () => {
+        optionsContainer.classList.toggle("active");
+    });
+});
+
+optionsList.forEach(o => {
+    o.addEventListener("click", () => {
+        selected.innerHTML = o.querySelector("label").innerHTML;
+        optionsContainer.classList.remove("active");
+    });
+});
