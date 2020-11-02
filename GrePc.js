e1581db2e93d267dfb4aa094439cd4368e4e7b8f
GrePC <64420953+GrePC@users.noreply.github.com>
2020-11-01 22:45:04 -0600

    Added login to hamburger

--- GrePc.js
+++ GrePc.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector(".menu-btn").addEventListener("click", function () {
+    document.querySelector(".burger").addEventListener("click", function () {
         document.querySelectorAll('*[class^="menu"]').forEach(item => item.classList.toggle("show"))
     })
 })
